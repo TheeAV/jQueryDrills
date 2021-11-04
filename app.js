10e6d58067e5b8322038f2383d881ff8651d91e0
TheeAV <89742988+TheeAV@users.noreply.github.com>
2021-11-04 17:42:38 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-// $(document).ready(function () {
-    $("<div></div>").appendTo("body");
+$(document).ready(function () {
+    
+    // $("<div></div>").appendTo("body");
+    $('<ul></ul>').appendTo('body');
 
     $("#btnSubmit").on('click', function (e) {
         e.preventDefault();
         alert("js is working");
         let $text = $('input[type="text"]').val();
-        $("<h2>" + $text + "</h2>").appendTo("div");
+        // $("<h2>" + $text + "</h2>").appendTo("div");
+        $('<li>' + $text + '</li>').appendTo('ul');
+        onClick();
         alert($text);
+        remove();
     });
 
     $(':input[type="submit"]').prop('disabled', true);
@@ -15,20 +20,30 @@
             $(':input[type="submit"]').prop('disabled', false);
         }
     });
+    // $('div').hover(function(){
+    //     $('div').mouseover(function(){
+    //         $('h2').css('color', 'red')
+    //     });
+    //     $('div').mouseleave(function(){
+    //         $('h2').css('color', 'black')
+    //     });
+    // });
+    function onClick() {
+        $('li').click(function () {
+            let color = ['red', 'blue', 'green', 'purple', 'yellow'];
+            let newColor = Math.floor(Math.random() * color.length);
+            $(this).css('color', color[newColor]);
+        })
+    };
 
-    
-$('div').hover(function(){
-    $('div').mouseover(function(){
-        $('h2').css('color', 'green')
-    });
-    $('div').mouseleave(function(){
-        $('h2').css('color', 'black')
-    });
-})
+    function remove() {
+        $('li').dblclick(function () {
+            $(this).remove();
+        });
+    };
+
+});
 
 
 
 
-
-
-// });
