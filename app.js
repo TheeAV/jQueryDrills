83c2dce2497e19dfc9bd722216ecce4df5c411ea
TheeAV <89742988+TheeAV@users.noreply.github.com>
2021-11-04 06:28:07 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
-    $("#btnSubmit").click(function () {
+ $("<div></div>").appendTo("body");
+ 
+ $("#btnSubmit").click(function () {
         alert("js is working");
+        let $text = $(".iText").val();
+        $("<h2>" + "#input" + "</h2>").appendTo("div");
+        alert($text);  
     });
+
+    $(':input[type="submit"]').prop('disabled', true);
+    $('input[type="text"]').keyup(function () {
+        if ($(this).val() != '') {
+            $(':input[type="submit"]').prop('disabled', false);
+        }
+    });
+    
+
+   
+
+
+    
+
+
+
 });
